d88d45afabeba5c9392ad470d1676144d807a3da
Eduardo Araújo <eduardojoaoaraujo@gmail.com>
2019-05-19 16:56:56 +0100

    Updates

---
+++ precache-manifest.7b56d8cde222cd12eb68d4be5cec5778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5b4a24ed398286359b",
+    "url": "/ColorOrganizer/static/js/runtime~main.de266475.js"
+  },
+  {
+    "revision": "bd247fad4a6f29dce0e4",
+    "url": "/ColorOrganizer/static/js/main.b8a2db43.chunk.js"
+  },
+  {
+    "revision": "05cf95c1cdf5004a7537",
+    "url": "/ColorOrganizer/static/js/2.1d5feb5a.chunk.js"
+  },
+  {
+    "revision": "bd247fad4a6f29dce0e4",
+    "url": "/ColorOrganizer/static/css/main.4d1813d0.chunk.css"
+  },
+  {
+    "revision": "044cc6d6c91060cd7d1f2f5d22aaf6a6",
+    "url": "/ColorOrganizer/index.html"
+  }
+];
